715c9cfe0573540dbf4fefb3676fff9a88c207e9
Alex <110457897+akaaja2@users.noreply.github.com>
2022-08-12 10:33:32 +0100

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,14 @@
+console.log("testing from the .js method");
+
+
+
+// https://www.theodinproject.com/lessons/foundations-fundamentals-part-1 exercises //
+
+console.log(23 + 97);
+console.log(23 + 97 + 3 + 5 + 6);
+let x = (4 + 6 + 9) / 77;
+
+
+
+
+
